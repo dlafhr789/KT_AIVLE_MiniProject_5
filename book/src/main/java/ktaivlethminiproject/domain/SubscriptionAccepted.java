7a7d032e21ba6909401d06c1005e9fdaebad9f0a
thlee <thlee@gmail.com>
2025-06-30 10:40:18 +0900

    SubscriptionAccepted

--- book/src/main/java/ktaivlethminiproject/domain/SubscriptionAccepted.java
+++ book/src/main/java/ktaivlethminiproject/domain/SubscriptionAccepted.java
@@ -4,15 +4,28 @@ import java.util.*;
 import ktaivlethminiproject.domain.*;
 import ktaivlethminiproject.infra.AbstractEvent;
 import lombok.*;
+//import java.time.LocalDateTime
 
-@Data
+//@Data
+//@ToString
+//public class SubscriptionAccepted extends AbstractEvent {
+//
+//    private Long id;
+//    private Long bookId;
+//    private Long userId;
+//    private String userName;
+//    private String state;
+//    private Date expiredAt;
+//}
+
+// 구독됨 정책
+@Getter
 @ToString
-public class SubscriptionAccepted extends AbstractEvent {
-
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscriptionAccepted {
     private Long id;
-    private Long bookId;
     private Long userId;
-    private String userName;
-    private String state;
-    private Date expiredAt;
+    private Boolean state;
+//    private LocalDateTime expiredAt;
 }
