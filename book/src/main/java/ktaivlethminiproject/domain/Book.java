4299c42273a1f3cf8e76bf934d0a6d4f6d87a939
thlee <thlee@gmail.com>
2025-06-30 10:04:22 +0900

    Book

--- book/src/main/java/ktaivlethminiproject/domain/Book.java
+++ book/src/main/java/ktaivlethminiproject/domain/Book.java
@@ -30,71 +30,25 @@ public class Book {
     private Boolean state = false;
     private Integer view = 0;
     private Integer subscribers = 0;
-
     private String imageUrl;  // 임록님만 믿겠습니다 이미지  url 화이팅
 
-//    @PostPersist
-//    public void onPostPersist() {
-//        PublicationRequested publicationRequested = new PublicationRequested(
-//            this
-//        );
-//        publicationRequested.publishAfterCommit();
-//
-//        BookSaved bookSaved = new BookSaved(this);
-//        bookSaved.publishAfterCommit();
-//
-//        BookOpenFailed bookOpenFailed = new BookOpenFailed(this);
-//        bookOpenFailed.publishAfterCommit();
-//
-//        Published published = new Published(this);
-//        published.publishAfterCommit();
-//
-//        IncreasedSubscriber increasedSubscriber = new IncreasedSubscriber(this);
-//        increasedSubscriber.publishAfterCommit();
-//    }
-
-    public static BookRepository repository() {
-        BookRepository bookRepository = BookApplication.applicationContext.getBean(
-            BookRepository.class
-        );
-        return bookRepository;
-    }
-
-    public void openBook(OpenBookCommand openBookCommand) {
+    public void openBook() {
         this.view++;
-//        repository().save(this);
 
         BookOpened bookOpened = new BookOpened(this);
         bookOpened.publishAfterCommit();
     }
 
-    public void publish(GenerateRequestCompleted generateRequestCompleted) {
-        Book book = new Book();
-        book.setTitle(generateRequestCompleted.getGeneratedTitle());
-        book.setContent(generateRequestCompleted.getGeneratedContent());
-        book.setUserId(generateRequestCompleted.getUserId());
-        book.setState("PUBLISHED");
-        book.setPublishedAt(new Date());
-        book.setImageUrl(generateRequestCompleted.getImageUrl()); // URL 사용
-        repository().save(book);
+    public void publish(String imageUrl) {
+        this.imageUrl = imageUrl;
+        this.state = true;
+        this.publishedAt = LocalDateTime.now();
 
-        Published pub = new Published(book);
-        pub.publishAfterCommit();
+        Published published = new Published(this);
+        published.publishAfterCommit();
     }
 
-    public static void subscribed(SubscriptionAccepted subscriptionAccepted) {
-        repository().findById(subscriptionAccepted.getBookId()).ifPresent(book -> {
-            book.setSubscribers(
-                (book.getSubscribers() == null) ? 1 : book.getSubscribers() + 1
-            );
-            repository().save(book);
-
-            IncreasedSubscriber inc = new IncreasedSubscriber(book);
-            inc.publishAfterCommit();
-        });
-    }
-
-    public void increaseSubscribers() {
+    public void Subscribed() {
         this.subscribers++;
 
         // 실제 구독자 증가 행위가 일어났을 때 이벤트 발행
