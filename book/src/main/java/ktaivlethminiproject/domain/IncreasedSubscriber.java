37a16a20b32d7791b9abaadb9b0d0d8ba7d2911e
thlee <thlee@gmail.com>
2025-06-30 10:33:15 +0900

    IncreasedSubscriber

--- book/src/main/java/ktaivlethminiproject/domain/IncreasedSubscriber.java
+++ book/src/main/java/ktaivlethminiproject/domain/IncreasedSubscriber.java
@@ -1,37 +1,34 @@
 package ktaivlethminiproject.domain;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 import ktaivlethminiproject.domain.*;
 import ktaivlethminiproject.infra.AbstractEvent;
 import lombok.*;
 
-//<<< DDD / Domain Event
-@Data
+@Getter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class IncreasedSubscriber extends AbstractEvent {
 
     private Long id;
-    private String title;
-    private String content;
     private Long userId;
-    private Date publishedAt;
-    private Integer view;
     private Integer subscribers;
+//    private String title;
+//    private String content;
+//    private LocalDateTime publishedAt;
+//    private Integer view;
+
 
     public IncreasedSubscriber(Book aggregate) {
-        super(aggregate);
+        super();
         this.id = aggregate.getId();
         this.userId = aggregate.getUserId();
-        this.title = aggregate.getTitle();
-        this.content = aggregate.getContent();
-        this.publishedAt = aggregate.getPublishedAt();
-        this.view = (aggregate.getView() == null) ? 0 : aggregate.getView();
-        this.subscribers = (aggregate.getSubscribers() == null) ? 0 : aggregate.getSubscribers();
-    }
-
-    public IncreasedSubscriber() {
-        super();
+        this.subscribers = aggregate.getSubscribers();
+//        this.title = aggregate.getTitle();
+//        this.content = aggregate.getContent();
+//        this.publishedAt = aggregate.getPublishedAt();
+//        this.view = aggregate.getView();
     }
 }
-//>>> DDD / Domain Event
