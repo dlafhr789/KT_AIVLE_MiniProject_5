39cb4f202ee4f7568e475d9b18595c14a2ed842f
thlee <thlee@gmail.com>
2025-06-30 10:13:58 +0900

    Published

--- book/src/main/java/ktaivlethminiproject/domain/Published.java
+++ book/src/main/java/ktaivlethminiproject/domain/Published.java
@@ -1,34 +1,33 @@
 package ktaivlethminiproject.domain;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 import ktaivlethminiproject.domain.*;
 import ktaivlethminiproject.infra.AbstractEvent;
 import lombok.*;
 
-//<<< DDD / Domain Event
 @Data
-@ToString
+@EqualsAndHashCode(callSuper=false)
 public class Published extends AbstractEvent {
 
     private Long id;
     private String title;
-    private String content;
-    private Long userId;
-    private String state;
-    private Integer view;
-    private Date publishedAt;
+//    private String content;
+//    private Long userId;
+//    private Boolean state;
+//    private Integer view;
+    private LocalDateTime publishedAt;
     private String imageUrl;  // 임로기가 줄 이미지 url~~~~
 
-
     public Published(Book aggregate) {
-        super(aggregate);
+        super();
+
         this.id = aggregate.getId();
         this.title = aggregate.getTitle();
-        this.content = aggregate.getContent();
-        this.userId = aggregate.getUserId();
-        this.state = aggregate.getState();
-        this.view = (aggregate.getView() == null) ? 0 : aggregate.getView();
+//        this.content = aggregate.getContent();
+//        this.userId = aggregate.getUserId();
+//        this.state = aggregate.getState();
+//        this.view = (aggregate.getView() == null) ? 0 : aggregate.getView();
         this.publishedAt = aggregate.getPublishedAt();
         this.imageUrl = aggregate.getImageUrl();  // Book 엔티티의 imageUrl 사용
     }
@@ -37,4 +36,3 @@ public class Published extends AbstractEvent {
         super();
     }
 }
-//>>> DDD / Domain Event
