2a8653711929f39674467cee834ce79517dc23df
imrok jeon <dlafhr789@naver.com>
2025-06-30 01:31:53 +0000

    feat: ai 표지 생성 기능 구현

--- bookpublish/src/main/java/ktaivlethminiproject/infra/PolicyHandler.java
+++ bookpublish/src/main/java/ktaivlethminiproject/infra/PolicyHandler.java
@@ -2,6 +2,11 @@ package ktaivlethminiproject.infra;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 import javax.naming.NameParser;
 import javax.naming.NameParser;
 import javax.transaction.Transactional;
@@ -21,9 +26,16 @@ import com.openai.models.chat.completions.ChatCompletionCreateParams;
 import com.openai.models.images.Image;
 import com.openai.models.images.ImageGenerateParams;
 import com.openai.models.images.ImageModel;
+import com.openai.models.images.ImagesResponse;
+import com.openai.models.images.ImageGenerateParams.Style;
 import com.openai.models.responses.Response;
 import com.openai.models.responses.ResponseCreateParams;
 
+import java.nio.file.Path;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Base64;
 // dotenv
 import io.github.cdimascio.dotenv.Dotenv;
 
@@ -93,28 +105,55 @@ public class PolicyHandler {
          * 스포일러나 완전한 시작-중간 아크는 절대 포함하지 마세요. 
          * 생생하면서도 간결한 언어(한국어로 2~4개의 짧은 문장)를 사용하면 사람들이 계속 읽고 싶게 만드는 훅으로 끝납니다.
          */
-        final String summary_prompt = "You are a top-tier Korean copywriter who writes irresistible teaser blurbs for books. Your goal is to spark the reader’s curiosity without revealing the full plot structure. Never include spoilers or the complete beginning-middle-end arc. Use vivid yet concise language (2-4 short sentences in Korean), end with a hook that makes people want to keep reading.";
+        final String summaryPrompt = "You are a top-tier Korean copywriter who writes irresistible teaser blurbs for books. Your goal is to spark the reader’s curiosity without revealing the full plot structure. Never include spoilers or the complete beginning-middle-end arc. Use vivid yet concise language (2-4 short sentences in Korean), end with a hook that makes people want to keep reading.";
 
-        ChatCompletionCreateParams params = ChatCompletionCreateParams.builder()
+        ChatCompletionCreateParams summaryParams = ChatCompletionCreateParams.builder()
             .model(ChatModel.GPT_4O_MINI)
-            .addSystemMessage(summary_prompt)
+            .addSystemMessage(summaryPrompt)
             .addUserMessage("Title : \n" + bookTitle + "\n\nContent : \n" + bookContent)
             .temperature(0.7)
             .build();
 
-        ChatCompletion chat = client.chat().completions().create(params);
+        ChatCompletion chat = client.chat().completions().create(summaryParams);
         String answer = chat.choices().get(0).message().content().orElse("empty");
         
 
         // 2. AI 표지 생성
 
-        // final String cover_prompt = "";
+        final String coverPrompt = "";
 
-        // ImageGenerateParams params = ImageGenerateParams.builder()
-        //     .model(ImageModel.DALL_E_3.asString())
-        //     .
+        ImageGenerateParams coverParams = ImageGenerateParams.builder()
+            .model("dall-e-3")
+            .prompt(coverPrompt)
+            .n(1)
+            .size(ImageGenerateParams.Size._1024X1536)  // 4:6 비율
+            .style(ImageGenerateParams.Style.VIVID)
+            .quality(ImageGenerateParams.Quality.HD)
+            .responseFormat(ImageGenerateParams.ResponseFormat.B64_JSON)
+            .build();
 
-        // GenData.requireAccepted(event);
+
+        List<Image> images = client.images().generate(coverParams).data().orElseThrow();
+
+        // 저장 경로 + 이미지 이름
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        String createdImagePath = "book_covers/" + userId + now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) + ".png";
+
+        for (int i = 0; i < images.size(); i++) {
+            // B64 문자열 -> PNG 바이트
+            String b64 = images.get(i).b64Json().orElseThrow();
+            byte[] png = Base64.getDecoder().decode(b64);
+
+            // 파일로 저장
+            Path out = Path.of(createdImagePath);
+            try {
+                Files.write(out, png);
+            } catch (IOException err) {
+                System.out.println("AI Generated Book Cover Write Error : " + err);
+            }
+        }
+
+        GenData.requireAccepted(event);
     }
 }
 //>>> Clean Arch / Inbound Adaptor
