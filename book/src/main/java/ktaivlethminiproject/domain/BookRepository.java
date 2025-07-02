16442ee7e4d584a6091d03fdb8356cfde477267e
thlee17 <lmnxyz271@gmail.com>
2025-07-02 06:09:43 +0000

    BookRepository

--- book/src/main/java/ktaivlethminiproject/domain/BookRepository.java
+++ book/src/main/java/ktaivlethminiproject/domain/BookRepository.java
@@ -3,9 +3,9 @@ package ktaivlethminiproject.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 //import ktaivlethminiproject.domain.*;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "books", path = "books")
+// @RepositoryRestResource(collectionResourceRel = "books", path = "books")
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findBySubscriberIdsContaining(Long userId);
 
